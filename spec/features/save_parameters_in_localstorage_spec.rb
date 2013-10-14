5205e6dee966635375836c9fbc1635fdcd271c9d
Hugues Marilleau <hugues@marilleau.com>
2013-10-14 20:17:00 +0200

    Add test for all form parameters save in localStorage

--- spec/features/save_parameters_in_localstorage_spec.rb
+++ spec/features/save_parameters_in_localstorage_spec.rb
@@ -1,14 +1,20 @@
-feature 'Save server URL in localStorage', :js => true do
-  scenario 'Put server URL in form and process form' do
+feature 'Save form parameters in localStorage', :js => true do
+  scenario 'Input form parameters and process form' do
     visit '/'
-    fill_in 'uri', :with => 'http://localhost:7474/'
+    fill_in 'uri',          :with => 'http://localhost:7474/'
+    fill_in 'query',        :with => 'START n=node({id}) RETURN n'
+    fill_in 'node_id',      :with => '1'
     click_on 'Run query'
     Capybara.reset!
   end
 
-  scenario 'Check that server URL is back from localStorage' do
+  scenario 'Check that form parameters are back from localStorage' do
     visit '/'
     find_field('uri').value == 'http://localhost:7474/'
     page.evaluate_script("localStorage.removeItem('uri')");
+    find_field('query').value == 'START n=node({id}) RETURN n'
+    page.evaluate_script("localStorage.removeItem('query')");
+    find_field('node_id').value == '1'
+    page.evaluate_script("localStorage.removeItem('node_id')");
   end
 end
